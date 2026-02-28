f15ee9cd2a2970fdb0865f2da22b9927a3f6ccde
Grant Canty <gc933815@ohio.edu>
2026-02-28 23:12:57 +0100

    cleaned up files. moved environmet to a dedicated folder with a __init__.py file

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='LOB-snapshot-backtesting',
     version='0.0.1',
     description='gymnasium environment for testing 1 second snap shots of limit order books',
-    url='git@github.com:GrantCanty/LOB-snapshot-backtesting.git',
+    url='https://github.com/GrantCanty/LOB-snapshot-backtesting.git',
     author='Grant Canty',
     author_email='gc933815@ohio.edu',
-    licence='unlicense',
-    packages=['environment'],
+    license='unlicense',
+    packages=find_packages(),
+    install_requires=[
+        'gymnasium',
+        'stable-baselines3',
+        'numpy',
+        'torch',
+        'pytz'
+    ]
 )
